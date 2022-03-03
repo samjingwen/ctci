1e6d62695819a6cb739e928a7c051f946274dcae
samjingwen <samjingwen@u.nus.edu>
2022-03-03 20:23:18 +0800

    Code, Code and More Code

--- playground.go
+++ playground.go
@@ -1,103 +1,43 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
+import "log"
+
+type User struct {
+	Name  string
+	Email string
+}
+type Admin struct {
+	User
+	Level string
+}
+type Notifier interface {
+	Notify() error
+}
+
+func (u *User) Notify() error {
+	log.Printf("User: Sending User Email To %s<%s>\n",
+		u.Name,
+		u.Email)
+
+	return nil
+}
+
+func (u *Admin) Notify() error {
+	log.Printf("Admin: Sending Admin Email To %s<%s>\n",
+		u.Name,
+		u.Email)
+
+	return nil
+}
+
+func SendNotification(notify Notifier) error {
+	return notify.Notify()
+}
 
 func main() {
-	//const nihongo = "日本語\u0099"
-	//const stuff = "abc"
-	//for index, runeValue := range nihongo {
-	//	fmt.Printf("%#U starts at byte position %d\n", runeValue, index)
-	//}
-	//
-	//for index, runeValue := range stuff {
-	//	fmt.Printf("%#U starts at byte position %d\n", runeValue, index)
-	//}
-
-	//mask := 0b001
-	//bitVector := 0b100
-	//
-	//b := bitVector & mask
-	//
-	//a := bitVector & ^mask
-	//
-	//c := bitVector | mask
-	//
-	//fmt.Printf("%b\n", b)
-	//
-	//fmt.Printf("%b\n", a)
-	//
-	//fmt.Printf("%b\n", c)
-
-	//head := makeNodeFromSlice([]int{1, 2, 3, 4, 5})
-	//arr := makeSliceFromNode(head)
-	//fmt.Println(arr)
-	//
-	//fmt.Println(head.equals([]int{1, 2, 3, 4, 5}))
-	//fmt.Println(head.equals([]int{1, 2, 3, 5}))
-	//fmt.Println(head.equals([]int{1, 2, 3, 4, 5, 6}))
-	//fmt.Println(head.equals([]int{}))
-	//
-	//head.append(6)
-	//fmt.Println(makeSliceFromNode(head))
-
-	source := rand.NewSource(time.Now().UnixNano())
-	random := rand.New(source)
-
-	for i := 0; i < 100; i++ {
-		fmt.Println(random.Intn(2))
-	}
-
-}
-
-type Node struct {
-	data int
-	next *Node
-}
-
-func makeNodeFromSlice(arr []int) *Node {
-	head := Node{0, nil}
-	ptr := &head
-	for _, val := range arr {
-		curr := Node{val, nil}
-		ptr.next = &curr
-		ptr = ptr.next
-	}
-	return head.next
-}
-
-func makeSliceFromNode(head *Node) []int {
-	var arr []int
-	ptr := head
-	for ptr != nil {
-		arr = append(arr, ptr.data)
-		ptr = ptr.next
-	}
-	return arr
-}
-
-func (head *Node) equals(arr []int) bool {
-	ptr := head
-	for _, val := range arr {
-		if ptr == nil || ptr.data != val {
-			return false
-		}
-		ptr = ptr.next
-	}
-	if ptr != nil {
-		return false
-	}
-	return true
-}
-
-func (head *Node) append(data int) {
-	end := Node{data, nil}
-	ptr := head
-	for ptr.next != nil {
-		ptr = ptr.next
-	}
-	ptr.next = &end
+	admin := &Admin{
+		User:  User{Name: "john smith", Email: "john@email.com"},
+		Level: "super"}
+	SendNotification(admin)
+	admin.Notify()
 }
