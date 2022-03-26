6c3cbafd42bd1d515b7e16d558778090c68100bf
samjingwen <samjingwen@u.nus.edu>
2022-03-26 20:09:38 +0800

    Code, Code and More Code

--- playground.go
+++ playground.go
@@ -2,15 +2,32 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
+func worker(id int) {
+	fmt.Printf("Worker %d starting\n", id)
+	time.Sleep(time.Second)
+	fmt.Printf("Worker %d done\n", id)
+}
+
 func main() {
-	var x uint32 = 0xaaaaaaaa
-	var y uint32 = 0x55555555
-	fmt.Printf("%b\n", x)
-	fmt.Printf("%b\n", y)
-	fmt.Printf("%b\n", 0xa)
-	fmt.Printf("%b\n", 5)
+	var wg sync.WaitGroup
+
+	for i := 1; i <= 5; i++ {
+		wg.Add(i)
+
+		i := i
+
+		go func() {
+			defer wg.Done()
+			worker(i)
+		}()
+	}
+
+	wg.Wait()
+
 }
 
 func getBit(num uint32, i int) bool {
