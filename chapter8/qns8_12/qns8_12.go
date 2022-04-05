8ccc9096c6e43b9d1d9a96f37587f8d054790ac6
samjingwen <samjingwen@u.nus.edu>
2022-04-05 19:55:33 +0800

    Always Be Coding

--- chapter8/qns8_12/qns8_12.go
+++ chapter8/qns8_12/qns8_12.go
@@ -1,49 +1,58 @@
 package qns8_12
 
-func NQueens(n int) [][]int {
-	gridSize := n
-	var res [][]int
+func NQueens(n int) [][]string {
+	var res [][]string
 
-	var iter func(row int, cols []int)
-	iter = func(row int, cols []int) {
-		if row == gridSize {
-			newCols := make([]int, len(cols))
-			copy(newCols, cols)
-			res = append(res, newCols)
+	var iter func(row int, diagonals, antiDiagonals, cols map[int]struct{}, state [][]rune)
+	iter = func(row int, diagonals, antiDiagonals, cols map[int]struct{}, state [][]rune) {
+		if row == n {
+			res = append(res, createGrid(state))
 			return
 		}
-		for col := 0; col < gridSize; col++ {
-			if checkValid(cols, row, col) {
-				cols[row] = col
-				iter(row+1, cols)
+
+		for col := 0; col < n; col++ {
+			currDiagonal := row - col
+			currAntiDiagonal := row + col
+
+			if _, ok := cols[col]; ok {
+				continue
 			}
+			if _, ok := diagonals[currDiagonal]; ok {
+				continue
+			}
+			if _, ok := antiDiagonals[currAntiDiagonal]; ok {
+				continue
+			}
+
+			cols[col] = struct{}{}
+			diagonals[currDiagonal] = struct{}{}
+			antiDiagonals[currAntiDiagonal] = struct{}{}
+			state[row][col] = 'Q'
+
+			iter(row+1, diagonals, antiDiagonals, cols, state)
+
+			delete(cols, col)
+			delete(diagonals, currDiagonal)
+			delete(antiDiagonals, currAntiDiagonal)
+			state[row][col] = '.'
 		}
 	}
 
-	iter(0, make([]int, 8))
+	emptyBoard := make([][]rune, n)
+	for i := 0; i < n; i++ {
+		emptyBoard[i] = make([]rune, n)
+		for j, _ := range emptyBoard[i] {
+			emptyBoard[i][j] = '.'
+		}
+	}
+	iter(0, make(map[int]struct{}), make(map[int]struct{}), make(map[int]struct{}), emptyBoard)
 	return res
 }
 
-func checkValid(cols []int, row1 int, col1 int) bool {
-	for row2 := 0; row2 < row1; row2++ {
-		col2 := cols[row2]
-
-		if col1 == col2 {
-			return false
-		}
-
-		columnDistance := abs(col2, col1)
-		rowDistance := row1 - row2
-		if columnDistance == rowDistance {
-			return false
-		}
+func createGrid(state [][]rune) []string {
+	var grid []string
+	for _, row := range state {
+		grid = append(grid, string(row))
 	}
-	return true
-}
-
-func abs(a, b int) int {
-	if a > b {
-		return a - b
-	}
-	return b - a
+	return grid
 }
