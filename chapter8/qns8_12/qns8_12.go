3139ad5ba887a3b9e1b6c7c0567f753189d62735
samjingwen <samjingwen@u.nus.edu>
2022-04-05 19:00:44 +0800

    Code, Code and More Code

--- chapter8/qns8_12/qns8_12.go
+++ chapter8/qns8_12/qns8_12.go
@@ -1,5 +1,49 @@
 package qns8_12
 
-func EightQueens() {
+func NQueens(n int) [][]int {
+	gridSize := n
+	var res [][]int
 
+	var iter func(row int, cols []int)
+	iter = func(row int, cols []int) {
+		if row == gridSize {
+			newCols := make([]int, len(cols))
+			copy(newCols, cols)
+			res = append(res, newCols)
+			return
+		}
+		for col := 0; col < gridSize; col++ {
+			if checkValid(cols, row, col) {
+				cols[row] = col
+				iter(row+1, cols)
+			}
+		}
+	}
+
+	iter(0, make([]int, 8))
+	return res
+}
+
+func checkValid(cols []int, row1 int, col1 int) bool {
+	for row2 := 0; row2 < row1; row2++ {
+		col2 := cols[row2]
+
+		if col1 == col2 {
+			return false
+		}
+
+		columnDistance := abs(col2, col1)
+		rowDistance := row1 - row2
+		if columnDistance == rowDistance {
+			return false
+		}
+	}
+	return true
+}
+
+func abs(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
 }
---
+++ chapter8/qns8_12/qns8_12_test.go
@@ -0,0 +1,24 @@
+package qns8_12
+
+import (
+	"testing"
+)
+
+func TestNQueens(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		args args
+		size int
+	}{
+		{args{8}, 92},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			if got := NQueens(tt.args.n); len(got) != tt.size {
+				t.Errorf("NQueens() = %v, want %v", got, tt.size)
+			}
+		})
+	}
+}
